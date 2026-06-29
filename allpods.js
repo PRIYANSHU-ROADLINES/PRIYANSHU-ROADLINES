b5faa2aed1bd9886f40ffb53e1b058fdc708a950
PRIYANSHU-ROADLINES <priyanshuroadlines611@gmail.com>
2026-06-29 19:29:03 +0530

    Update allpods.js

--- allpods.js
+++ allpods.js
@@ -16,3 +16,17 @@ import {
   serverTimestamp,
   writeBatch
 } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-firestore.js";
+
+// Firebase Config
+const firebaseConfig = {
+  apiKey: "AIzaSyBQZREq5abr_oLzt6ksMGb-1jhlnKc92pU",
+  authDomain: "priyanshu-roadlines-pod.firebaseapp.com",
+  projectId: "priyanshu-roadlines-pod",
+  storageBucket: "priyanshu-roadlines-pod.firebasestorage.app",
+  messagingSenderId: "735411516260",
+  appId: "1:735411516260:web:397d6a80141f032c0a0071"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
