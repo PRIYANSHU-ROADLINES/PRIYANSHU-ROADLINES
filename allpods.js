740d799ebacb55cb6f700894806022d05d44b025
PRIYANSHU-ROADLINES <priyanshuroadlines611@gmail.com>
2026-06-29 19:28:06 +0530

    Update allpods.js

--- allpods.js
+++ allpods.js
@@ -1,2 +1,18 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-app.js";
-
+import {
+  getFirestore,
+  doc,
+  setDoc,
+  updateDoc,
+  getDoc,
+  deleteDoc,
+  collection,
+  getDocs,
+  addDoc,
+  orderBy,
+  query,
+  limit,
+  where,
+  serverTimestamp,
+  writeBatch
+} from "https://www.gstatic.com/firebasejs/12.14.0/firebase-firestore.js";
