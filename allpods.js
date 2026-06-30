9ce41f662b2e31fd864922b0130549b462808770
PRIYANSHU-ROADLINES <priyanshuroadlines611@gmail.com>
2026-06-30 17:37:10 +0530

    Update allpods.js

--- allpods.js
+++ allpods.js
@@ -176,3 +176,63 @@ box-shadow:0 2px 10px rgba(0,0,0,.2);
 };
 window.searchPod = window.searchPod;
 window.filterStatus = window.filterStatus;
+window.searchByDate = async function () {
+
+    const fromDate = document.getElementById("fromDate").value;
+    const toDate = document.getElementById("toDate").value;
+
+    if (!fromDate || !toDate) {
+        alert("Please select both dates");
+        return;
+    }
+
+    const snapshot = await getDocs(collection(db, "pods"));
+
+    const container = document.getElementById("allPodsContainer");
+
+    container.innerHTML = "";
+
+    snapshot.forEach((docItem) => {
+
+        const pod = docItem.data();
+
+        if (
+            pod.deliveryDate >= fromDate &&
+            pod.deliveryDate <= toDate
+        ) {
+
+            container.innerHTML += `
+
+<div style="
+background:white;
+padding:15px;
+margin:15px 0;
+border-radius:10px;
+box-shadow:0 2px 10px rgba(0,0,0,.2);
+">
+
+<b>GR No:</b> ${pod.grNo}</br>
+
+<b>Party:</b> ${pod.partyName || "-"}</br>
+
+<b>Vehicle:</b> ${pod.vehicleNo || "-"}</br>
+
+<b>Status:</b> ${pod.status}</br><br>
+
+<button onclick="window.location.href='index.html?gr=${pod.grNo}'">
+👁 View POD
+</button>
+
+</div>
+
+`;
+
+        }
+
+    });
+
+    if (container.innerHTML === "") {
+        container.innerHTML = "<h3>No POD found in selected date range.</h3>";
+    }
+
+};
