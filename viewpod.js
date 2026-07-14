761efeced8357c44a2adaeb334bb64ac5f6f028e
PRIYANSHU-ROADLINES <priyanshuroadlines611@gmail.com>
2026-07-14 19:45:57 +0530

    Create viewpod.js

---
+++ viewpod.js
@@ -0,0 +1,80 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-app.js";
+
+import {
+  getFirestore,
+  doc,
+  getDoc
+} from "https://www.gstatic.com/firebasejs/12.14.0/firebase-firestore.js";
+
+// Firebase Config
+const firebaseConfig = {
+  apiKey: "AIzaSyBQZREq5abr_oLzt6ksMGb-1jhlnKc92pU",
+  authDomain: "priyanshu-roadlines-pod.firebaseapp.com",
+  projectId: "priyanshu-roadlines-pod",
+  storageBucket: "priyanshu-roadlines-pod.firebasestorage.app",
+  messagingSenderId: "735411516260",
+  appId: "1:735411516260:web:397d6a80141f032c0a0071"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+// Read GR Number from URL
+const params = new URLSearchParams(window.location.search);
+const grNo = params.get("gr");
+
+if (!grNo) {
+
+    document.getElementById("result").innerHTML =
+    "<h2>No GR Number Found.</h2>";
+
+} else {
+
+    loadPOD(grNo);
+
+}
+
+async function loadPOD(grNo){
+
+    const snap = await getDoc(doc(db,"pods",grNo));
+
+    if(!snap.exists()){
+
+        document.getElementById("result").innerHTML =
+        "<h2>POD Not Found</h2>";
+
+        return;
+
+    }
+
+    const data = snap.data();
+
+    document.getElementById("result").innerHTML = `
+
+<h2>GR Number : ${data.grNo}</h2>
+
+<p><b>Party :</b> ${data.partyName || "-"}</p>
+
+<p><b>Vehicle :</b> ${data.vehicleNo || "-"}</p>
+
+<p><b>Driver :</b> ${data.driverName || "-"}</p>
+
+<p><b>Mobile :</b> ${data.driverMobile || "-"}</p>
+
+<p><b>Delivery Date :</b> ${data.deliveryDate || "-"}</p>
+
+<p><b>Status :</b> ${data.status || "-"}</p>
+
+<p><b>Remarks :</b> ${data.remarks || "-"}</p>
+
+<img src="${data.imageUrl}" alt="POD">
+
+<br><br>
+
+<a href="${data.imageUrl}" target="_blank">
+Download POD
+</a>
+
+`;
+
+}
