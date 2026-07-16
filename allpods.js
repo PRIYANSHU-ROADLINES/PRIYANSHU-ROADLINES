c67c1e5d37acaafbee5704bbc1ba92a50913b2b0
PRIYANSHU-ROADLINES <priyanshuroadlines611@gmail.com>
2026-07-16 10:33:27 +0530

    Update allpods.js

--- allpods.js
+++ allpods.js
@@ -30,6 +30,10 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 const db = getFirestore(app);
+let allPods = [];
+let filteredPods = [];
+let currentPage = 1;
+const recordsPerPage = 10;
 
 
 
