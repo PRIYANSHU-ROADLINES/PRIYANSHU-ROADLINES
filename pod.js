047080c00eb79fb8486922f9727c5cb375c78797
PRIYANSHU-ROADLINES <priyanshuroadlines611@gmail.com>
2026-07-20 11:22:38 +0530

    Update pod.js

--- pod.js
+++ pod.js
@@ -41,12 +41,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 const auth = getAuth(app);
-
-
-
-
-
-
+signOut(auth);
 const db = getFirestore(app);
 
 let isAdminLoggedIn = false;
