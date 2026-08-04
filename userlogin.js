437ab292c10cbcf7b4cafe05e6c7f847eb3edf89
PRIYANSHU-ROADLINES <priyanshuroadlines611@gmail.com>
2026-08-04 14:49:36 +0530

    Update userlogin.js

--- userlogin.js
+++ userlogin.js
@@ -1,1 +1,13 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-app.js";
 
+import {
+    getAuth,
+    createUserWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/12.14.0/firebase-auth.js";
+
+import {
+    getFirestore,
+    doc,
+    setDoc,
+    serverTimestamp
+} from "https://www.gstatic.com/firebasejs/12.14.0/firebase-firestore.js";
