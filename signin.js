a5798033c43bb16b4533c2c15d0c3840cf5a3508
PRIYANSHU-ROADLINES <priyanshuroadlines611@gmail.com>
2026-08-04 14:32:27 +0530

    Update signin.js

--- signin.js
+++ signin.js
@@ -1,1 +1,15 @@
+document.getElementById("continueBtn").addEventListener("click", function () {
 
+    const role = document.querySelector('input[name="role"]:checked').value;
+
+    if (role === "user") {
+
+        window.location.href = "userlogin.html";
+
+    } else {
+
+        window.location.href = "operatorlogin.html";
+
+    }
+
+});
