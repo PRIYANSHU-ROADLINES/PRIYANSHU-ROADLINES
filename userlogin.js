5ae3b65bb84b58175423229705830058c9861675
PRIYANSHU-ROADLINES <priyanshuroadlines611@gmail.com>
2026-08-04 14:51:00 +0530

    Update userlogin.js

--- userlogin.js
+++ userlogin.js
@@ -11,3 +11,56 @@ import {
     setDoc,
     serverTimestamp
 } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-firestore.js";
+const firebaseConfig = {
+  apiKey: "AIzaSyBQZREq5abr_oLzt6ksMGb-1jhlnKc92pU",
+  authDomain: "priyanshu-roadlines-pod.firebaseapp.com",
+  projectId: "priyanshu-roadlines-pod",
+  storageBucket: "priyanshu-roadlines-pod.firebasestorage.app",
+  messagingSenderId: "735411516260",
+  appId: "1:735411516260:web:397d6a80141f032c0a0071"
+};
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+const db = getFirestore(app);
+
+document.getElementById("signupBtn").addEventListener("click", signupCustomer);
+
+async function signupCustomer(){
+
+const mobile =
+document.getElementById("mobile").value.trim();
+
+const fullname =
+document.getElementById("fullname").value.trim();
+
+const email =
+document.getElementById("email").value.trim();
+
+const password =
+document.getElementById("password").value;
+
+if(!/^[6-9]\d{9}$/.test(mobile)){
+
+alert("Enter a valid 10 digit Mobile Number.");
+
+return;
+
+}
+
+if(fullname.length < 3){
+
+alert("Enter Full Name.");
+
+return;
+
+}
+
+if(password.length < 8){
+
+alert("Password must be at least 8 characters.");
+
+return;
+
+}
