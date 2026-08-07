bc5ddf85f0a4c51a3f170d69d7452f37c2369f8b
PRIYANSHU-ROADLINES <priyanshuroadlines611@gmail.com>
2026-08-07 20:04:24 +0530

    Create pod-security.js

---
+++ pod-security.js
@@ -0,0 +1,20 @@
+const operatorLoggedIn =
+localStorage.getItem("loggedIn") === "true";
+
+const role =
+localStorage.getItem("role");
+
+if (!operatorLoggedIn) {
+
+    alert("Please sign in first.");
+
+    window.location.replace("signin.html");
+
+}
+else if (role !== "staff" && role !== "admin") {
+
+    alert("You are not authorized to access POD.");
+
+    window.location.replace("index.html");
+
+}
