c028d28ca7dd41d1a2d130b53cfdac3e650bdd89
PRIYANSHU-ROADLINES <priyanshuroadlines611@gmail.com>
2026-08-11 17:59:00 +0530

    Update pod.js

--- pod.js
+++ pod.js
@@ -40,7 +40,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 const auth = getAuth(app);
-signOut(auth);
+signOut(podAuth);
 
 const podAuthApp = initializeApp(firebaseConfig, "POD_AUTH_APP");
 const podAuth = getAuth(podAuthApp);
