a4cab26d699c59a66421c04e52d11fd0ff48e5bd
PRIYANSHU-ROADLINES <priyanshuroadlines611@gmail.com>
2026-08-25 16:29:35 +0530

    Create securityAlerts.js

---
+++ js/security/securityAlerts.js
@@ -0,0 +1,48 @@
+import {
+    getFirestore,
+    doc,
+    setDoc,
+    serverTimestamp
+} from "https://www.gstatic.com/firebasejs/12.14.0/firebase-firestore.js";
+
+
+// ============================================
+// SECURITY ALERT
+// ============================================
+
+export async function createSecurityAlert({
+    email,
+    mobile,
+    fullname,
+    attempts
+}) {
+
+    const db = getFirestore();
+
+    const alertRef = doc(
+        db,
+        "securityAlerts",
+        "operatorVerificationAlert"
+    );
+
+    await setDoc(alertRef, {
+
+        type: "5 Failed Operator Login Attempts",
+
+        attempts: attempts,
+
+        email: email,
+
+        mobile: mobile,
+
+        fullname: fullname,
+
+        timestamp: serverTimestamp(),
+
+        status: "New",
+
+        blocked: false
+
+    });
+
+}
