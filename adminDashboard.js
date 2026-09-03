6fecbc9a550c0d461d079ebd3cf6808403aa8c96
PRIYANSHU-ROADLINES <priyanshuroadlines611@gmail.com>
2026-09-03 16:05:32 +0530

    Update adminDashboard.js

--- adminDashboard.js
+++ adminDashboard.js
@@ -7,7 +7,10 @@ import {
     query,
     orderBy,
     doc,
-    updateDoc
+    updateDoc,
+    setDoc,
+    deleteDoc,
+    serverTimestamp
 }
 from "https://www.gstatic.com/firebasejs/12.14.0/firebase-firestore.js";
 
